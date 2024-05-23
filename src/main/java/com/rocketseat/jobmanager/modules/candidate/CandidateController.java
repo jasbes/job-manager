839baea7e6fcec2a0e5f85e9fc92158594ff6848
Jefferson <jefferson_uk@hotmail.com>
2024-05-23 23:13:10 +0200

    Initial project setup

---
+++ src/main/java/com/rocketseat/jobmanager/modules/candidate/CandidateController.java
@@ -0,0 +1,8 @@
+package com.rocketseat.jobmanager.modules.candidate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/candidates")
+public class CandidateController {
+}
