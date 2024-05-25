7107e62fd5a3ab830db390c6ed0bd8f9d5535635
Jefferson <jefferson_uk@hotmail.com>
2024-05-26 01:56:31 +0200

    Add git actions badge

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Job manager
+
 Project developed following the training provided by [Rocketseat](https://www.rocketseat.com.br/).
+
+[![Java CI with Maven](https://github.com/jasbes/job-manager/actions/workflows/ci.yml/badge.svg)](https://github.com/jasbes/job-manager/actions/workflows/ci.yml)
--- src/main/java/com/rocketseat/jobmanager/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/rocketseat/jobmanager/modules/candidate/controller/CandidateController.java
@@ -1,8 +1,10 @@
-package com.rocketseat.jobmanager.modules.candidate;
+package com.rocketseat.jobmanager.modules.candidate.controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/candidates")
 public class CandidateController {
+
 }
